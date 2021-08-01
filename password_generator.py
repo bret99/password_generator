f2bc3664f3361c6525f5255e47b1641df119b977
bret99 <64787351+bret99@users.noreply.github.com>
2021-08-01 05:11:55 -0600

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -10,28 +10,22 @@ try:
     length = int(input("Enter Password length: "))
     if int(length) < 8:
         sys.exit("\033[1;91mPassword length should be more than 8 characters!\033[1;00m")
+    for i in range(int(length/3)):
+        password += random.choice(letters)
+        password += random.choice(digits)
+        password += random.choice(special_symbols)
+
     if int(length)%3 == 2:
-        for i in range(int(length/3)):
-            password += random.choice(letters)
-            password += random.choice(digits)
-            password += random.choice(special_symbols)
         password += random.choice(letters)
         password += random.choice(digits)
 
     if int(length)%3 == 1:
-        for i in range(int(length/3)):
-            password += random.choice(letters)
-            password += random.choice(digits)
-            password += random.choice(special_symbols)
         password += random.choice(letters)
 
-    if int(length)%3 == 0:
-        for i in range(int(length/3)):
-            password += random.choice(letters)
-            password += random.choice(digits)
-            password += random.choice(special_symbols)
-
-    print(password)
+    password_to_list = list(password)
+    random.shuffle(password_to_list)
+    random_password = "".join(str(i) for i in password_to_list)
+    print(random_password)
 
 except ValueError:
     sys.exit("\033[1;91mWrong input!\033[;00m")
